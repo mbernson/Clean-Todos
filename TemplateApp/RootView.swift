d7ecff24ba071ec414be161b5dd09738dcb48f56
Mathijs Bernson <mathijs.bernson@gmail.com>
2024-09-04 14:13:47 +0200

    Initial commit

---
+++ TemplateApp/RootView.swift
@@ -0,0 +1,20 @@
+//
+//  RootView.swift
+//  TemplateApp
+//
+//  Copyright © 2024 Q42. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        NavigationView {
+            HomeScreen()
+        }
+    }
+}
+
+#Preview {
+    RootView()
+}
